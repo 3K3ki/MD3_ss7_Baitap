91d55b1e8df0a6777a0adac46ad880c51d401b9e
3K3ki <danghung090@gmail.com>
2023-03-17 10:04:53 +0700

    commit 4

--- src/baitap1/Circle.java
+++ src/baitap1/Circle.java
@@ -1,24 +1,19 @@
 package baitap1;
 
-public class Circle extends Shape {
+public class Circle extends Shape implements Resizeable {
     private double radius = 1.0;
-
     public Circle() {
     }
-
     public Circle(double radius) {
         this.radius = radius;
     }
-
     public Circle(double radius, String color, boolean filled) {
         super(color, filled);
         this.radius = radius;
     }
-
     public double getRadius() {
         return radius;
     }
-
     public void setRadius(double radius) {
         this.radius = radius;
     }
@@ -30,7 +25,6 @@ public class Circle extends Shape {
     public double getPerimeter() {
         return 2 * radius * Math.PI;
     }
-
     @Override
     public String toString() {
         return "A Circle with radius="
--- src/baitap1/Main.java
+++ src/baitap1/Main.java
@@ -3,26 +3,21 @@ import java.util.Arrays;
 import java.util.Comparator;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("------------------Resizeable-------------------");
-        Shape[] shapes = new Shape[3];
-        shapes[0] = new Circle(3.6);
-        shapes[1] = new Rectangle();
-        shapes[2] = new Square(3.5, "indigo", false);
+        Shape[] shape = new Shape[3];
+        shape[0] = new Circle();
+        shape[1] = new Rectangle();
+        shape[2] = new Square(5);
 
-        System.out.println("resizeable:");
-        for (Shape sh : shapes) {
-            if(sh instanceof Circle){
+        for(Shape sh : shape){
+            if (sh instanceof Circle){
+               ;
+                ((Circle) sh).resize(Math.random());
                 System.out.println(sh);
-            ((Circle)sh).resize(Math.random());
-            System.out.println(sh);
+            } else if (sh instanceof Rectangle) {
+
+                ((Rectangle) sh).resize(Math.random());
+                System.out.println("sau khi tăng: "+sh);
+            }
         }
-    }
-
-
-        System.out.println("After-resizeable:");
-        for (Shape sh : shapes) {
-            System.out.println(sh);
-        }
-
     }
 }
