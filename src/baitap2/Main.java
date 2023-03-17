7f01a1f9460fef1b041d99669b1784633ca55e23
3K3ki <danghung090@gmail.com>
2023-03-17 09:07:00 +0700

    commit2

---
+++ src/baitap2/Main.java
@@ -0,0 +1,18 @@
+package baitap2;
+import java.util.Arrays;
+import java.util.Comparator;
+public class Main {
+    public static void main(String[] args) {
+        Shape[] shape = new Shape[4];
+        shape[0] = new Circle();
+        shape[1] = new Rectangle();
+        shape[2] = new Square(5);
+        for (Shape sh : shape) {
+            System.out.println(sh);
+            if (sh instanceof Square) {
+                ((Square) sh).howToColor();
+            }
+        }
+
+    }
+}
